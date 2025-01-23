29012a67acf4dedc9e89e3d4a32c758cf9cc0424
SadmanWalid <40543547+SadmanWalid@users.noreply.github.com>
2025-01-23 17:22:51 +0600

    Add Role and User models for security layer.

---
+++ PROJECT.Model/Security/Role.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PROJECT.Model.Security
+{
+    [Table("Roles", Schema = "Sec")]
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+
+    }
+}
---
+++ PROJECT.Model/Security/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT.Model.Security
+{
+    [Table("Users", Schema = "Sec")]
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LoginId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+    }
+}
