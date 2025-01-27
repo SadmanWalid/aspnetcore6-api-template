3cfb75ef029ab5d6de7f8de7db7ac2c2c93ace7f
SadmanWalid <40543547+SadmanWalid@users.noreply.github.com>
2025-01-27 12:14:26 +0600

    Add ApplicationDbContext with DbSet for User and Role entities.

---
+++ PROJECT.Database/Database/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PROJECT.Model.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT.Database.Database
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        // Add DbSet for each entity
+
+        #region Security
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        #endregion
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("Users", "Sec");
+            modelBuilder.Entity<Role>().ToTable("Roles", "Sec");
+        }
+
+    }
+}
