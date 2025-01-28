1e80a78a32cba9bc9fbb33bda365dbf557da8c7d
SadmanWalid <40543547+SadmanWalid@users.noreply.github.com>
2025-01-28 14:25:37 +0600

    Add JWT authentication, CORS policy, and dependency registration in Program.cs

--- PROJECT.API/Program.cs
+++ PROJECT.API/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using PROJECT.API.DependencyInjection;
+using System.Text;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// connection string
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 
@@ -6,6 +15,50 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// Add Dependency Resolver
+Services.RegisterDependencies(builder.Services, connectionString);
+
+//Add Reference Cycle Ignore
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+}).AddXmlDataContractSerializerFormatters();
+
+//Add JWT authentication
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.SaveToken = true;
+    x.RequireHttpsMetadata = false;
+    x.TokenValidationParameters = new TokenValidationParameters()
+    {
+
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidAudience = jwtSettings["Audience"], // Issued By Backend Service or Port
+        ValidIssuer = jwtSettings["Issuer"], // Listen By Frontend Service or Port
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+// cors policy
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy", build =>
+{
+    build.SetIsOriginAllowed(isOriginAllowed: _ => true).AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithExposedHeaders("Content-Disposition");
+    //https://scottgeek.technology/dotnet-6-api-with-a-httpdelete-and-that-damn-405-not-allowed/
+
+}));
 
 var app = builder.Build();
 
@@ -15,6 +68,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
