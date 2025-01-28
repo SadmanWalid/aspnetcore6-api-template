f6d371f89eb63b2d9e9add2e369ebf5c73ee239b
SadmanWalid <40543547+SadmanWalid@users.noreply.github.com>
2025-01-28 12:06:22 +0600

    Change return type of GetAsync method in IGenericRepository to IEnumerable<T>

--- PROJECT.IRepository/Infrastructure/IGenericRepository.cs
+++ PROJECT.IRepository/Infrastructure/IGenericRepository.cs
@@ -8,7 +8,7 @@ namespace PROJECT.IRepository.Infrastructure
 {
     public  interface IGenericRepository<T> where T : class
     {
-        Task<ICollection<T>> GetAsync();
+        Task<IEnumerable<T>> GetAsync();
         Task<T> GetByIdAsync(int id);
         bool AddAsync(T entity);
         bool UpdateAsync(T entity);
