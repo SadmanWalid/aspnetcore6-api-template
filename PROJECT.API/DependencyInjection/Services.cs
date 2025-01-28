d034215c83c5054ce017ba175a5411dc38aa1086
SadmanWalid <40543547+SadmanWalid@users.noreply.github.com>
2025-01-28 14:25:09 +0600

    Add dependency injection setup and remove unused folder references

---
+++ PROJECT.API/DependencyInjection/Services.cs
@@ -0,0 +1,16 @@
+﻿using PROJECT.IService.Infrastructure;
+using PROJECT.Service.Infrastructure;
+
+namespace PROJECT.API.DependencyInjection
+{
+    public static class Services
+    {
+        public static IServiceRegistration serviceRegistration{ get; set; } = new ServiceRegistration();
+
+        public static void RegisterDependencies(IServiceCollection services, string database)
+        {
+            serviceRegistration.AddInfrastructure(services, database);
+        }
+
+    }
+}
