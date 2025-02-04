9f7922a8b2556ef967df87447c9b12e6e9ccb4f0
SadmanWalid <40543547+SadmanWalid@users.noreply.github.com>
2025-02-04 11:57:32 +0600

    Add AutoMapper profile for mapping between User and UserDTO

---
+++ PROJECT.API/AutoMapper/ModelDTOMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PROJECT.DTO.Security;
+using PROJECT.Model.Security;
+
+namespace PROJECT.API.AutoMapper
+{
+    public class ModelDTOMapper: Profile
+    {
+        public ModelDTOMapper()
+        {
+
+            CreateMap<User, UserDTO>();
+            CreateMap<UserDTO, User>();
+
+        }
+    }
+}
