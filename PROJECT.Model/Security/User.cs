419a9a89f5ed1ea948902d3acf8dbf69d71cdd48
SadmanWalid <40543547+SadmanWalid@users.noreply.github.com>
2025-02-04 11:56:45 +0600

    Update User model to include CreatedBy and ModifiedBy fields, and make CreatedDate nullable.

--- PROJECT.Model/Security/User.cs
+++ PROJECT.Model/Security/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Net.Http.Headers;
@@ -17,9 +18,12 @@ namespace PROJECT.Model.Security
         public string Email { get; set; }
         public string Password { get; set; }
         public bool IsActive { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int? ModifiedBy { get; set; }
         public DateTime? ModifiedDate { get; set; }
         public int RoleId { get; set; }
+
         public virtual Role Role { get; set; }
     }
 }
